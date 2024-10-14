95f43e192bca673ac00a9093cbb84325f424837d
Freddy <Freddy@DESKTOP-CR8ETTA>
2024-10-13 21:38:19 -0500

    CompleteOddOrNot

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese Numero entero o 'S  para salir ");
+    numberString = Console.ReadLine();  //number = "45"
+    var numberInt = 0;
+    if (numberString!.ToLower() == "salir" )
+    {
+        continue;
+    }
+    if (int.TryParse(numberString, out numberInt!))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste : {numberString} no es un numero entero ");
+    }
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over");
+
+
+
